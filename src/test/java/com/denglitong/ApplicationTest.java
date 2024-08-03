d99a9426dbc32ac66ad5611e9379c93d9358291d
Litong Deng <litongdeng@gmail.com>
2024-08-03 10:55:08 +0800

    Initial commit
    Signed-off-by: Litong Deng <litongdeng@gmail.com>

---
+++ src/test/java/com/denglitong/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.denglitong;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
